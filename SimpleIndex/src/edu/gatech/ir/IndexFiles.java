4e2f1678582f9b337865d41838c8a72afee29719
little sheep <juyuanyang@gmail.com>
2013-01-23 15:28:14 -0500

    1st version of project code

---
+++ SimpleIndex/src/edu/gatech/ir/IndexFiles.java
@@ -0,0 +1,112 @@
+package edu.gatech.ir;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.FieldInfo.IndexOptions;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class IndexFiles {
+
+	public static void main(String[] args) {
+		String docsPath = "docs";
+		String indexPath = "index";
+		boolean create = true;
+
+		final File docDir = new File(docsPath);
+		if (!docDir.exists() || !docDir.canRead()) {
+			System.out
+					.println("Document directory does not exist or is not readable, please check the path");
+			System.exit(1);
+		}
+
+		Date start = new Date();
+		try {
+			System.out.println("Indexing to directory '" + indexPath + "'...");
+
+			Directory dir = FSDirectory.open(new File(indexPath));
+			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+
+			if (create) {
+				// Create a new index in the directory, removing any previously indexed documents:
+				iwc.setOpenMode(OpenMode.CREATE);
+			} else {
+				// Add new documents to an existing index:
+				iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+			}
+
+			IndexWriter writer = new IndexWriter(dir, iwc);
+			indexDocs(writer, docDir);
+
+			writer.close();
+
+			Date end = new Date();
+			System.out.println(end.getTime() - start.getTime()
+					+ " total milliseconds");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void indexDocs(IndexWriter writer, File file) throws IOException {
+		// do not try to index files that cannot be read
+		if (file.canRead()) {
+			if (file.isDirectory()) {
+				String[] files = file.list();
+				if (files != null) { // an IO error could occur
+					for (int i = 0; i < files.length; i++) {
+						indexDocs(writer, new File(file, files[i]));
+					}
+				}
+			} else {
+				FileInputStream fis;
+				try {
+					fis = new FileInputStream(file);
+				} catch (FileNotFoundException fnfe) {
+					return;
+				}
+
+				try {
+					// make a new, empty document
+					Document doc = new Document();
+
+					Field pathField = new Field("filename", file.getName(),
+							Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+					pathField.setIndexOptions(IndexOptions.DOCS_ONLY);
+					doc.add(pathField);
+
+					doc.add(new Field("contents", new BufferedReader(
+							new InputStreamReader(fis, "UTF-8"))));
+
+					if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+						System.out.println("adding " + file);
+						writer.addDocument(doc);
+					} else {
+						System.out.println("updating " + file);
+						writer.updateDocument(new Term("path", file.getPath()), doc);
+					}
+
+				} finally {
+					fis.close();
+				}
+			}
+		}
+	}
+
+}
---
+++ SimpleIndex/src/edu/gatech/ir/SearchFiles.java
@@ -0,0 +1,52 @@
+package edu.gatech.ir;
+
+import java.io.File;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class SearchFiles {
+
+	private SearchFiles() {}
+	
+	public static void main(String[] args) {
+		String index = "index";
+	    String field = "contents";
+	    String toSearch = "exception";
+	    int maxHits = 10;
+	    
+	    IndexReader reader;
+		try {
+			reader = IndexReader.open(FSDirectory.open(new File(index)));
+		    IndexSearcher searcher = new IndexSearcher(reader);
+		    Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+		    QueryParser parser = new QueryParser(Version.LUCENE_31, field, analyzer);
+		    
+		    Query query = parser.parse(toSearch);
+		    System.out.println("Searching for: " + query.toString(field));
+		    
+		    TopDocs results = searcher.search(query, 5 * maxHits);
+		    ScoreDoc[] hits = results.scoreDocs;
+		    
+		    for (int i = 0; i < hits.length; i++) {
+		        Document doc = searcher.doc(hits[i].doc);
+		       	String filename = doc.get("filename");
+		       	
+		       	System.out.println("doc="+hits[i].doc+" score="+hits[i].score + " name="+filename);
+		    }
+		    searcher.close();
+		    reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
